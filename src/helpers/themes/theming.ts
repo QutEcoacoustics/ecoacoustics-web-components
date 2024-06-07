19683909d90bcc288fb64201f5f7440a9af5febd
hudson-newey <grath@duck.com>
2024-06-07 10:06:43 +1000

    staging for build changes

--- src/helpers/themes/theming.ts
+++ src/helpers/themes/theming.ts
@@ -5,10 +5,10 @@ export const theming = css`
   :host,
   :root {
     --oe-theme-hue: 247deg;
-    --oe-theme-saturation: 57%;
-    --oe-theme-lightness: 91%;
+    --oe-theme-saturation: 27%;
+    --oe-theme-lightness: 95%;
 
-    --oe-background-color: hsl(var(--oe-theme-hue), var(--oe-theme-saturation), calc(var(--oe-theme-lightness) + 42%));
+    --oe-background-color: hsl(var(--oe-theme-hue), var(--oe-theme-saturation), calc(var(--oe-theme-lightness) + 22%));
     --oe-font-color: hsl(
       var(--oe-theme-hue),
       calc(var(--oe-theme-saturation) - 50%),
@@ -16,16 +16,20 @@ export const theming = css`
     );
 
     --oe-border-rounding: 6px;
+    --oe-border-color: hsl(var(--oe-theme-hue), var(--oe-theme-saturation), calc(var(--oe-theme-lightness) - 10%));
     --oe-font-family: sans-serif;
-    --oe-box-shadow: 1px 1px 2px currentcolor;
+    --oe-box-shadow: 1px 1px 1px currentcolor;
 
     --oe-primary-color: hsl(var(--oe-theme-hue), var(--oe-theme-saturation), calc(var(--oe-theme-lightness) - 38%));
     --oe-secondary-color: hsl(var(--oe-theme-hue), var(--oe-theme-saturation), calc(var(--oe-theme-lightness) - 10%));
     --oe-accent-color: hsl(var(--oe-theme-hue), var(--oe-theme-saturation), calc(var(--oe-theme-lightness) - 20%));
     --oe-info-color: hsl(207deg, var(--oe-theme-saturation), calc(var(--oe-theme-lightness) - 15%));
-    --oe-danger-color: hsl(0deg, 90%, 61%);
+    /* --oe-danger-color: hsl(0deg, 90%, 61%); */
+    --oe-danger-color: color-mix(in srgb, hsl(0deg, 90%, 61%) 75%, var(--oe-background-color));
     --oe-selected-color: hsl(var(--oe-theme-hue), var(--oe-theme-saturation), calc(var(--oe-theme-lightness) - 5%));
-    --oe-panel-color: hsl(var(--oe-theme-hue), var(--oe-theme-saturation), calc(var(--oe-theme-lightness) + 5%));
+    --oe-panel-color: hsl(var(--oe-theme-hue), var(--oe-theme-saturation), calc(var(--oe-theme-lightness) + 2%));
+
+    --oe-panel-color-lighter: color-mix(in srgb, var(--oe-panel-color) 1%, var(--oe-background-color));
   }
 
   * {
@@ -103,22 +107,33 @@ export const theming = css`
     margin-bottom: 1.5rem;
   }
 
+  dialog {
+  }
+
   input:not([type="checkbox"], [type="radio"]),
   textarea,
   select {
+    box-shadow: 0 0 0 1px var(--oe-secondary-color);
+
+    &:focus {
+      box-shadow: 0 0 0 2px var(--oe-selected-color);
+    }
+  }
+
+  input,
+  textarea,
+  select {
     font-size: 1rem;
-    padding: 0.6em;
+    padding: 0.56em;
     padding-left: 1em;
     border: none;
     border-radius: var(--oe-border-rounding);
-    background-color: var(--oe-panel-color);
-    box-shadow: 0 0 0 2px var(--oe-secondary-color);
+    background-color: var(--oe-panel-color-lighter);
     color: var(--oe-font-color);
 
     &:focus {
       outline: none;
       border-color: var(--oe-selected-color);
-      box-shadow: 0 0 0 2px var(--oe-selected-color);
       background-color: var(--oe-background-color);
     }
   }
@@ -152,7 +167,8 @@ export const theming = css`
     &::file-selector-button {
       border: none;
       border-radius: var(--oe-border-rounding);
-      color: var(--oe-primary-color);
+      color: var(--oe-font-color);
+      font-weight: bold;
       background-color: var(--oe-panel-color);
       padding: 0.5rem;
     }
@@ -163,6 +179,9 @@ export const theming = css`
     https://css-tricks.com/how-to-recreate-the-ripple-effect-of-material-design-buttons/#aa-css-only
   */
   button {
+    --background-color: var(--oe-panel-color);
+    --ripple-color: color-mix(in srgb, var(--background-color) 90%, black);
+
     border: none;
     border-radius: var(--oe-border-rounding);
     margin: 0.1rem;
@@ -171,15 +190,25 @@ export const theming = css`
     padding: 1em;
     padding-left: 2rem;
     padding-right: 2rem;
-    transition: background 0.8s;
+    background-color: var(--background-color);
+    transition: background 0.3s;
+
+    /*
+      Box shadows make a button look raised and clickable.
+      Therefore, if the button is disabled, we do not want to show the box shadow.
+    */
+    &:not(:disabled) {
+      box-shadow: var(--oe-box-shadow);
+    }
 
     &:hover:not(:disabled) {
-      background: var(--oe-panel-color) radial-gradient(circle, transparent 1%, var(--oe-panel-color) 1%) center/15000%;
+      background: var(--background-color) radial-gradient(circle, transparent 1%, var(--background-color) 1%)
+        center/15000%;
       cursor: pointer;
     }
 
     &:active:not(:disabled) {
-      background-color: var(--oe-accent-color);
+      background-color: var(--ripple-color);
       background-size: 100%;
       transition: background 0s;
     }
@@ -190,20 +219,20 @@ export const theming = css`
   }
 
   .oe-btn-primary {
-    background-color: var(--oe-background-color);
+    --background-color: var(--oe-background-color);
     border: 2px solid var(--oe-primary-color);
   }
 
   .oe-btn-secondary {
-    background-color: var(--oe-secondary-color);
+    --background-color: var(--oe-secondary-color);
   }
 
   .oe-btn-info {
-    background-color: var(--oe-info-color);
+    --background-color: var(--oe-info-color);
   }
 
   .oe-btn-danger {
-    background-color: var(--oe-danger-color);
+    --background-color: var(--oe-danger-color);
     color: white;
   }
 `;
